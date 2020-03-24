936ae7073709057edc5ac7f6619382fafbb04029
Cody Asher <casher@oakland.edu>
2020-03-23 22:46:37 -0400

    THIS GONNA WORK

--- custom_card_creator/src/main/webapp/poke-script.js
+++ custom_card_creator/src/main/webapp/poke-script.js
@@ -26,7 +26,7 @@ function addText() {
     document.getElementById("retr").innerHTML = retreat;
     document.getElementById("etc").innerHTML = etc;
     
-    document.getElementById("cardtype").src="/custom-card-creator/resources/pokemonTemplates/" + type + ".png";
+    document.getElementById("cardtype").src="/custom-card-creator/resources/pokemonTemplates/" + type.toLowerCase() + ".png";
 }
 
 //showing image on card
