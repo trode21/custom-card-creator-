c1800afda4dd03d2514e8741a1ee17d33617aa3c
jbhatnagar <jaidev.bhatnagar@gmail.com>
2020-04-08 20:41:28 -0400

    Started on Magic Card image download factory, renamed some files

--- custom_card_creator/src/main/java/custom_card_creator/MagicFactory.java
+++ custom_card_creator/src/main/java/custom_card_creator/MagicFactory.java
@@ -6,29 +6,37 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class MagicFactory {
-	
-	public void buildImage(String templateName, String typeName, BufferedImage mainImage)
-	throws IOException
+@WebServlet("magicServlet")
+public class MagicFactory extends HttpServlet {
+	public void buildImage(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException
 	{
+		String templateName = "magic", colorName = request.getParameter("color");
+		BufferedImage mainImage = (BufferedImage) request.getAttribute("image");
+		String name = request.getParameter("name");
+		String type = request.getParameter("type");
+		String description = request.getParameter("description");
+		String quote = request.getParameter("quote");
+		String cost = request.getParameter("cost");
+		
+		
 		BufferedImage template = ImageIO.read(new File("/custom_card_creator/src/main/webapp/resources/"
-					+templateName+"Templates"+"/"+typeName+".png"));
+					+templateName+"Templates"+"/"+colorName+".png"));
 		int w = template.getHeight();
 		int h = template.getWidth();
 		
 		CardImage cImage = new CardImage(mainImage);
-		
-		CustomCardText cText = new CustomCardText(x,y,typeName,"Arial");
-		
 		BufferedImage builtImage = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
 		Graphics worker = builtImage.getGraphics();
 		worker.drawImage(template, 0, 0, null);
-		worker.drawString(templateName, 0,0);
+		worker.drawImage(cImage.resizeImage(430 , 320, cImage.getImage()), 46, 80, null);
 		
-		if("Magic".equals(templateName)) {
-			worker.drawImage(cImage.resizeImage(430 , 320, cImage.getImage()), 46, 80, null);
-		}
 		ImageIO.write(builtImage, "PNG", new File("/custom_card_creator/src/main/java/custom_card_creator/localCards/", "builtMagic.png"));
 	}
 }
