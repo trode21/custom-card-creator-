5b321e566627d53111f3f39774cbc946d4e18116
Jaidev Karan Bhatnagar <jaidev.bhatnagar@gmail.com>
2020-04-15 18:18:53 -0400

    deleted the old project

--- custom_card_creator/src/main/webapp/java/custom_card_creator/CustomCardCreatorConfiguration.java
+++
@@ -1,9 +0,0 @@
-/*
-package custom_card_creator;
-import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.validation.constraints.NotEmpty;
-public class CustomCardCreatorConfiguration extends Configuration {
-	
-}
-*/
